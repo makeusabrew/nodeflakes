49664721f76a05a1b621a840bee777cfb7dfa838
Nick Payne <nick@kurai.co.uk>
2011-11-23 13:36:06 +0000

    non working stream processor test

---
+++ tests/server/spec/StreamProcessorSpec.js
@@ -0,0 +1,18 @@
+var srcDir = __dirname+'/../../..';
+
+var StreamProcessor = require(srcDir+'/app/workers/stream_processor');
+
+describe('StreamConsumer', function() {
+    var processor;
+    beforeEach(function() {
+        processor = new StreamProcessor();
+    });
+
+    it('should emit an error message when processing invalid JSON', function() {
+        expect(
+            processor.process("Invalid JSON")
+        ).toThrow(
+            new Error("parse error of: Invalid JSON")
+        );
+    });
+});
